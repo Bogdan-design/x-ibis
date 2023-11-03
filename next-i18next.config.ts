e9d6e3bb9bf18b9ab841deff15b76ad15119b9ab
Bogdan-design <bogdanbw@gmail.com>
2023-11-03 15:03:55 +0100

    refactor

--- next-i18next.config.ts
+++ next-i18next.config.ts
@@ -17,24 +17,24 @@ module.exports = {
         locales: ['en', 'de','pl'],
     },
 
-    // // backend: {
-    // //     backendOptions: [{
-    // //         expirationTime: 60 * 60 * 1000 // 1 hour
-    // //     }, {
-    // //         projectId: 'd3b405cf-2532-46ae-adb8-99e88d876733',
-    // //         version: 'latest'
-    // //     }],
-    // //     backends: isBrowser ? [LocalStorageBackend, LocizeBackend] : [],
-    // // },
-    // serializeConfig: false,
-    // use: isBrowser ? [ChainedBackend] : [],
-    // /** To avoid issues when deploying to some paas (vercel...) */
-    // localePath:
-    //     typeof window === 'undefined'
-    //         ? require('path').resolve('./services')
-    //         : '/services',
-    //
-    // reloadOnPrerender: process.env.NODE_ENV === 'development',
+    backend: {
+        backendOptions: [{
+            expirationTime: 60 * 60 * 1000 // 1 hour
+        }, {
+            projectId: 'd3b405cf-2532-46ae-adb8-99e88d876733',
+            version: 'latest'
+        }],
+        backends: isBrowser ? [LocalStorageBackend, LocizeBackend] : [],
+    },
+    serializeConfig: false,
+    use: isBrowser ? [ChainedBackend] : [],
+    /** To avoid issues when deploying to some paas (vercel...) */
+    localePath:
+        typeof window === 'undefined'
+            ? require('path').resolve('./services')
+            : '/services',
+
+    reloadOnPrerender: process.env.NODE_ENV === 'development',
 
 
 }
