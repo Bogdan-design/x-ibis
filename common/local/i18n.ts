1be53b01d0946d8f18c7abb12f2ee4d6bf7f5f5e
Bogdan-design <bogdanbw@gmail.com>
2023-11-03 14:17:58 +0100

    refactor

--- common/local/i18n.ts
+++ common/local/i18n.ts
@@ -1,6 +1,5 @@
-// 'use client'
 import i18n from "i18next";
-import {initReactI18next} from "react-i18next";
+import { initReactI18next } from 'react-i18next';
 import en from '@/public/locales/en/common.json'
 import de from '@/public/locales/de/common.json'
 import pl from '@/public/locales/pl/common.json'
@@ -19,16 +18,12 @@ const resources ={
     }
 }
 
-// const getLng = ()=>{
-//     return String(localStorage.getItem('i18nextLng')) || Constants.EN
-// }
-//
 
 i18n
     .use(initReactI18next)
     .init({
         resources,
-        fallbackLng: Constants.EN,
+        fallbackLng: String(localStorage.getItem('i18nextLng')) || Constants.EN,
         interpolation: {
             escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
         }
@@ -36,3 +31,4 @@ i18n
 
 
 
+export default i18n;
